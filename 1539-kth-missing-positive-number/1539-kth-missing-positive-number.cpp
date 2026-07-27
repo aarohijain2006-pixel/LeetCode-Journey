014eb6b0c1a60e698b7188758b438febb900a0b0
Aarohi_Jain <aarohi.jain2006@gmail.com>
2026-07-27 17:56:26 +0530

    Time: 0 ms (100%), Space: 13.2 MB (93.19%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        vector<int>miss;
-        for(int i = 1; i<=arr.size() + k; i++){
-            if(!binary_search(arr.begin(), arr.end(), i)){
-                miss.push_back(i);
-            }
-        }
-        return miss[k-1];
+       int n = arr.size();
+       int low = 0;
+       int high = n-1;
+       while(low<=high){
+          int mid = low + (high - low)/2;
+          int missing = arr[mid] - (mid + 1);
+          if(missing < k){
+             low = mid + 1;
+          }
+          else{
+            high = mid - 1;
+          }
+       }
+       return low + k;
     }
 };
+// At the end ans = arr[high] + more, where more is k - missing(at the end). So missing = arr[high] - (high + 1). so, after putting values ans = high + 1+ k. And heigh = low -1. finally ans = low + k. 
